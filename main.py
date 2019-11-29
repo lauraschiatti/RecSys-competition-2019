413dc81c485476df7230a75e064f71c5d50305e5
lauricdd <lauri_cdd@hotmail.com>
2019-11-29 17:56:54 +0100

    submission UserCFKNN using split_train_validation_random_holdout

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ use_validation_set = False
 k_out_value = 1  # Leave One Out (keep 1 interaction/user)
 leave_random_out = True
 
-splitted_data = data_splitter.split_train_leave_k_out_user_wise(URM, k_out=k_out_value,
-                                                           use_validation_set=use_validation_set,
-                                                           leave_random_out=leave_random_out)
-
-# splitted_data = data_manager.split_train_validation_random_holdout(URM, train_split=0.8)
+# splitted_data = data_splitter.split_train_leave_k_out_user_wise(URM, k_out=k_out_value,
+#                                                            use_validation_set=use_validation_set,
+#                                                            leave_random_out=leave_random_out)
+#
+splitted_data = data_manager.split_train_validation_random_holdout(URM, train_split=0.8)
 
 
 if use_validation_set:
