55e5b3558006c4963b95fa50bb376dae1a94ba24
lauricdd <lauri_cdd@hotmail.com>
2019-12-12 18:29:57 +0100

    hyperparams tuning on ItemCBFKNNRecommender, training, evaluation and predictions

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 
 
 import os, traceback
-
-from utils.data_manager import build_URM
+import numpy as np
+from utils.data_manager import build_URM, build_ICM, get_target_users
 from utils.Evaluation.Evaluator import EvaluatorHoldout
-from utils.ParameterTuning.parameter_search import runParameterSearch_Collaborative
+from utils.ParameterTuning.hyperparameter_search import runParameterSearch_Collaborative, runParameterSearch_Content
 from utils.DataIO import DataIO
-from utils import create_submission_file as create_csv
+from utils.create_submission_file import create_csv
 from utils.data_splitter import split_train_validation_random_holdout
 from utils import masks
 
@@ -43,8 +43,7 @@ from recommenders.PureSVDRecommender import PureSVDRecommender
 ##########                  PURE CONTENT BASED              ##########
 ##########                                                  ##########
 ######################################################################
-# from KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
-
+from utils.KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
 
 ######################################################################
 ##########                                                  ##########
@@ -52,11 +51,9 @@ from recommenders.PureSVDRecommender import PureSVDRecommender
 ##########                                                  ##########
 ######################################################################
 
+URM_all = build_URM()
 
-# Build URM
-# ---------
-
-URM = build_URM()
+ICM_all = build_ICM()
 
 # URM statistics
 # --------------
@@ -72,7 +69,7 @@ URM = build_URM()
 
 
 # URM train/validation/test splitting
-# -------------------------------
+# -----------------------------------
 
 # from Data_manager.Movielens1M.Movielens1MReader import Movielens1MReader
 # from Data_manager.DataSplitter_k_fold_stratified import DataSplitter_Warm_k_fold
@@ -82,7 +79,7 @@ URM = build_URM()
 # dataSplitter.load_data()
 # URM_train, URM_validation, URM_test = dataSplitter.get_holdout_split()
 
-URM_train, URM_test = split_train_validation_random_holdout(URM, train_split=0.8)
+URM_train, URM_test = split_train_validation_random_holdout(URM_all, train_split=0.8)
 URM_train, URM_validation = split_train_validation_random_holdout(URM_train, train_split=0.9)
 
 # Tuning parameters
@@ -95,12 +92,15 @@ evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[10, 15])
 evaluator_validation_earlystopping = None  # EvaluatorHoldout(URM_train, cutoff_list=[5], exclude_seen = False)
 output_folder_path = "result_experiments/"
 
-n_cases = 8 # 2
-n_random_starts = 5 # 1
+n_cases = 8  # 2
+n_random_starts = 5  # 1
 
 save_model = "no"
-allow_weighting = False
+allow_weighting = False #todo: try with weighting
 similarity_type_list = ["cosine"]
+
+ICM_name = "ICM_all"
+
 
 # Collaborative recommenders
 collaborative_algorithm_list = [
@@ -117,9 +117,26 @@ collaborative_algorithm_list = [
     # SLIMElasticNetRecommender
 ]
 
-print('\nRecommender Systems: ')
-for i, recomm_type in enumerate(collaborative_algorithm_list, start=1):
-    print('{}. {}'.format(i, recomm_type.RECOMMENDER_NAME))
+# Content-based recommenders
+content_algorithm_list = [
+    ItemKNNCBFRecommender
+]
+
+recommender_list = [
+    # Random,
+    # TopPop,
+    # P3alphaRecommender,
+    # RP3betaRecommender,
+    ItemKNNCFRecommender,
+    UserKNNCFRecommender,
+    # MatrixFactorization_BPR_Cython,
+    # MatrixFactorization_FunkSVD_Cython,
+    PureSVDRecommender,
+    # SLIM_BPR_Cython,
+    # SLIMElasticNetRecommender,
+    ItemKNNCBFRecommender
+]
+
 
 # from Utils.PoolWithSubprocess import PoolWithSubprocess
 # import multiprocessing
@@ -130,17 +147,23 @@ for i, recomm_type in enumerate(collaborative_algorithm_list, start=1):
 # pool.join()
 
 
+print('\nRecommender Systems: ')
+for i, recomm_type in enumerate(recommender_list, start=1):
+    print('{}. {}'.format(i, recomm_type.RECOMMENDER_NAME))
+
 while True:
     try:
         selected = int(input('\nSelect a recommender system: '.format(i)))
-        recommender_class = collaborative_algorithm_list[selected - 1]
+        recommender_class = recommender_list[selected - 1]
         print('\n ... {} ... '.format(recommender_class.RECOMMENDER_NAME))
+
+        output_file_name_root = "{}_metadata.zip".format(recommender_class.RECOMMENDER_NAME)
+
 
         # Hyperparameters tuning
         if recommender_class in collaborative_algorithm_list:
 
             try:
-
                 runParameterSearch_Collaborative(recommender_class=recommender_class,
                                                  URM_train=URM_train,
                                                  metric_to_optimize=metric_to_optimize,
@@ -154,27 +177,54 @@ while True:
                                                  allow_weighting=allow_weighting,
                                                  similarity_type_list=similarity_type_list)
 
+                if recommender_class in [ItemKNNCFRecommender, UserKNNCFRecommender]:
+                    similarity_type = similarity_type_list[0]  # KNN Recommenders on similarity_type
+                    output_file_name_root = "{}_{}_metadata.zip".format(recommender_class.RECOMMENDER_NAME,
+                                                                        similarity_type)
+
+            except Exception as e:
+                print("On recommender {} Exception {}".format(recommender_class, str(e)))
+                traceback.print_exc()
+
+
+        if recommender_class in content_algorithm_list:
+            try:
+                runParameterSearch_Content(recommender_class=recommender_class,
+                                           URM_train=URM_train,
+                                           ICM_object=ICM_all,
+                                           ICM_name=ICM_name,
+                                           n_cases=n_cases,
+                                           n_random_starts=n_random_starts,
+                                           save_model=save_model,
+                                           evaluator_validation=evaluator_validation,
+                                           evaluator_test=evaluator_test,
+                                           metric_to_optimize=metric_to_optimize,
+                                           output_folder_path=output_folder_path,
+                                           allow_weighting=allow_weighting,
+                                           similarity_type_list=similarity_type_list)
+
+                similarity_type = similarity_type_list[0]  # KNN Recommenders on similarity_type
+                output_file_name_root = "{}_{}_{}_metadata.zip".format(recommender_class.RECOMMENDER_NAME,
+                                                                       ICM_name, similarity_type)
+
             except Exception as e:
                 print("On recommender {} Exception {}".format(recommender_class, str(e)))
                 traceback.print_exc()
 
 
         # Load best_parameters for training
-
-        if recommender_class in [ItemKNNCFRecommender, UserKNNCFRecommender]:
-            # KNN Recommenders on similarity_type
-            similarity_type = similarity_type_list[0]
-            output_file_name_root = recommender_class.RECOMMENDER_NAME + "_" + similarity_type + "_metadata.zip"
-        else:
-            output_file_name_root = recommender_class.RECOMMENDER_NAME + "_metadata.zip"
-
         data_loader = DataIO(folder_path=output_folder_path)
         search_metadata = data_loader.load_data(output_file_name_root)
         best_parameters = search_metadata["hyperparameters_best"]  # dictionary with all the fit parameters
-        print("best_parameters", best_parameters)
+        print("best_parameters {}".format(best_parameters))
 
         # Fit the recommender with the parameters we just learned
-        recommender = recommender_class(URM_train)
+        if recommender_class in content_algorithm_list:
+            # todo: ICM_all or ICM_train?
+            recommender = recommender_class(URM_train, ICM_all)
+        else:
+            recommender = recommender_class(URM_train)
+
         recommender.fit(**best_parameters)
 
         # Evaluate model
@@ -188,24 +238,24 @@ while True:
                             'y - Yes  n - No\n')
 
         if predictions == 'y':
-            print("predictions")
-        #     # Train the model on the whole dataset using tuned params
-        #     recommender = recommender_class(URM)
-        #     recommender.fit(**best_parameters)
-        #
-        #     top_10_items = {}
-        #     target_user_id_list = get_target_users()
-        #
-        #     for user_id in target_user_id_list:
-        #         item_list = ''
-        #         for item in range(10):  # recommended_items
-        #             item_list = recommender.recommend(user_id, cutoff=10)
-        #             item_list = np.array(item_list)  # list to np.array
-        #
-        #             top_10_items[user_id] = item_list  # .strip() # remove trailing space
-        #
-        #     # save predictions on csv file
-        #     create_csv(top_10_items, recomm_type)
+
+            # Train the model on the whole dataset using tuned params
+            recommender = recommender_class(URM_all)
+            recommender.fit(**best_parameters)
+
+            top_10_items = {}
+            target_user_id_list = get_target_users()
+
+            for user_id in target_user_id_list[0:10]:
+                item_list = ''
+                for item in range(10):  # recommended_items
+                    item_list = recommender.recommend(user_id, cutoff=10)
+                    item_list = np.array(item_list)  # list to np.array
+
+                    top_10_items[user_id] = item_list  # .strip() # remove trailing space
+
+            # save predictions on csv file
+            create_csv(top_10_items, recommender_class.RECOMMENDER_NAME)
 
         break
 
